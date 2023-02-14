a7598216d601c490219ea946c32879f7e49b5f5d
HunterOBrien <obrienh2@middleton.school.nz>
2023-02-15 10:30:13 +1300

    Create Taxi_trips.py

---
+++ Taxi_trips.py
@@ -0,0 +1,6 @@
+while True:
+    doYouWantToStartANewTrip = input("Do you want to START A NEW TRIP: ").capitalize()
+
+    if doYouWantToStartANewTrip == "NO":
+        quit("DO YOU WANT TO START A NEW TRIP: ")
+
