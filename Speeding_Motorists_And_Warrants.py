e15efdc92345eb9172caf8a4ac82aba2ebadb972
HunterOBrien <obrienh2@middleton.school.nz>
2023-02-22 10:30:14 +1300

    Create Speeding_Motorists_And_Warrants.py

---
+++ Speeding_Motorists_And_Warrants.py
@@ -0,0 +1,11 @@
+fineAmount = 0
+
+while True:
+    print("#######################")
+    name = input("Enter Name Of Speeder: ")
+    overLimit = int(input("How Many Kilometres Above The Speed Limit"))
+
+    if overLimit < 10:
+        fineAmount = 30
+
+    elif overLimit
